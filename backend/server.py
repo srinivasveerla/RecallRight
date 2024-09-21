521396abdd5404ba19c1ac0c942082bf1688c194
Prashant Godhwani <prashant.godhwani@hotmail.com>
2024-09-21 19:59:54 -0400

    Added api in URI

--- backend/server.py
+++ backend/server.py
@@ -5,20 +5,20 @@ from models.request import RecallRequest, QnABySearchQuery
 app = FastAPI()
 content_service = ContentProcessorService()
 
-@app.post("/data")
+@app.post("/api/data")
 async def read_root(request: RecallRequest):
     # raw_body = await request.body()
     # print(raw_body.decode())
     content_service.upsert(request)
     return {"message": "Data received successfully"}
 
-@app.get("/tags")
+@app.get("/api/tags")
 async def get_tags(count: int = 10):
     if(count > 100 or count < 0): count = 100
     return content_service.get_tags(count)
 
 
-@app.post("/questionsBySearchQuery")
+@app.post("/api/questionsBySearchQuery")
 async def get_questions_by_search_query(request: QnABySearchQuery):
     questions = content_service.questions_by_search_query(request)
     return questions
